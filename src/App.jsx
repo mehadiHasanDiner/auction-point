afcda60d99041e2d45884ea537157c38f8a68c8a
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2026-03-07 16:12:23 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p className="text-3xl font-bold ">hello</p>
+        <button className="btn btn-neutral">Neutral</button>
+        <button className="btn btn-primary">Primary</button>
+        <button className="btn btn-secondary">Secondary</button>
+        <button className="btn btn-accent">Accent</button>
+        <button className="btn btn-info">Info</button>
+        <button className="btn btn-success">Success</button>
+        <button className="btn btn-warning">Warning</button>
+        <button className="btn btn-error">Error</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
