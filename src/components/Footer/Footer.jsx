cd76bc60ac770c978d1fd3256b5a66193087a429
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2026-03-10 19:17:15 +0600

    all are done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,22 +2,29 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className=" mt-20">
-      <div className=" justify-center items-center flex flex-col bg-base-100 py-15">
-        <div className="flex">
-          <a className="text-2xl text-blue-700">
-            Auction
-            <span className="text-yellow-400 font-bold text-2xl">Point</span>
-          </a>
-        </div>
-        <div className="space-y-5">
-          <p>Bid. Win. Own.</p>
-          <div className="text-center">
-            <p>Home </p>
-            <p> Auctions </p>
-            <p> Categories </p>
-            <p> How to works</p>
+    <div className=" mt-20 bg-base-100 ">
+      <div className=" justify-center items-center flex py-15 ">
+        <div>
+          <div className="flex items-center justify-center">
+            <a className="text-2xl text-blue-700">
+              Auction
+              <span className="text-yellow-400 font-bold text-2xl">Point</span>
+            </a>
           </div>
+          <div className="space-y-5">
+            <div className="text-center">
+              <p>Bid. Win. Own.</p>
+              <ul className="flex space-x-5 mt-2">
+                <li>Home</li>
+                <li>Auctions</li>
+                <li>Categories</li>
+                <li>How to works</li>
+              </ul>
+            </div>
+          </div>
+          <p className="text-center mt-2">
+            © 2025 AuctionHub. All rights reserved.
+          </p>
         </div>
       </div>
     </div>
